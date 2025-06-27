0b4cd9043706e1368421f393ba95c0c079409f5a
German Castellanos <german.castellanos@guate5.com>
2025-06-27 12:18:29 -0600

    feat: ✨ new text

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Land App'),
+      home: const MyHomePage(title: 'Countries'),
     );
   }
 }
@@ -44,12 +44,11 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
+        titleTextStyle: TextStyle(fontSize: 40, color: Colors.black),
       ),
       body: Center(
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
